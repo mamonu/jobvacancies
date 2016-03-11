f478febeb4afdcbd9f712debae5836c512415125
mamonu <thorosm@msn.com>
2016-03-11 18:31:11 +0000

    reed API too

--- jobvac.r
+++ jobvac.r
@@ -1,12 +1,28 @@
 #library(jsonlite)
 #library(RJSONIO)
-
+#library(httr)    # for GET(...)
 library(jsonlite)
 library(formattable)
+library(RCurl)
 
-json_file <- "http://api.lmiforall.org.uk/api/v1/vacancies/search?keywords=cook"
+#json_file <- "http://api.lmiforall.org.uk/api/v1/vacancies/search?keywords=cook"
 
-result <-(fromJSON((json_file)))
+
+
+#You will need to include your api key 
+#for all requests in a basic authentication http header as the username, leaving the password empty.
+
+
+
+query <- "http://www.reed.co.uk/api/1.0/search?keywords=accountant&location=london&distancefromlocation=15"
+#getdata<-GET(url=query, authenticate("a3296058-fe85-4e6e-934e-50f2f0f78f88", "")) 
+
+reedjson<- getURL(query, userpwd="a3296058-fe85-4e6e-934e-50f2f0f78f88:", httpauth = 1L)
+
+
+result<-(fromJSON( reedjson ))
+
+#result <-(fromJSON((json_file)))
 
 formattable(result, list())
 
